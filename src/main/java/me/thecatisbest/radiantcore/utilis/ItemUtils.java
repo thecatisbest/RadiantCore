be62287785a19cc60ad1149b611fc7236e547f35
Paper Airplane <102031694+thecatisbest@users.noreply.github.com>
2024-07-17 17:31:47 +0800

    Fix

--- src/main/java/me/thecatisbest/radiantcore/utilis/ItemUtils.java
+++ src/main/java/me/thecatisbest/radiantcore/utilis/ItemUtils.java
@@ -1,6 +1,6 @@
 package me.thecatisbest.radiantcore.utilis;
 
-import me.thecatisbest.radiantcore.config.GeneralConfig;
+import me.thecatisbest.radiantcore.config.ConfigValue;
 import me.thecatisbest.radiantcore.utilis.builder.ItemBuilder;
 import org.bukkit.Material;
 
@@ -15,22 +15,22 @@ public class ItemUtils {
 
     public ItemUtils() {
         this.magic_mushroom_soup = new ItemBuilder(Material.PLAYER_HEAD);
-        this.magic_mushroom_soup.setItemName(Utilis.color(GeneralConfig.MAGIC_MUSHROOM_SOUP_NAME));
-        this.magic_mushroom_soup.setItemLore(Utilis.color(GeneralConfig.MAGIC_MUSHROOM_SOUP_LORE));
-        this.magic_mushroom_soup.texture(GeneralConfig.MAGIC_MUSHROOM_SOUP_TEXTURE);
+        this.magic_mushroom_soup.setItemName(Utilis.color(ConfigValue.MAGIC_MUSHROOM_SOUP_NAME));
+        this.magic_mushroom_soup.setItemLore(Utilis.color(ConfigValue.MAGIC_MUSHROOM_SOUP_LORE));
+        this.magic_mushroom_soup.texture(ConfigValue.MAGIC_MUSHROOM_SOUP_TEXTURE);
 
         this.super_magic_mushroom_soup = new ItemBuilder(Material.PLAYER_HEAD);
-        this.super_magic_mushroom_soup.setItemName(Utilis.color(GeneralConfig.SUPER_MAGIC_MUSHROOM_SOUP_NAME));
-        this.super_magic_mushroom_soup.setItemLore(Utilis.color(GeneralConfig.SUPER_MAGIC_MUSHROOM_SOUP_LORE));
-        this.super_magic_mushroom_soup.texture(GeneralConfig.SUPER_MAGIC_MUSHROOM_SOUP_TEXTURE);
+        this.super_magic_mushroom_soup.setItemName(Utilis.color(ConfigValue.SUPER_MAGIC_MUSHROOM_SOUP_NAME));
+        this.super_magic_mushroom_soup.setItemLore(Utilis.color(ConfigValue.SUPER_MAGIC_MUSHROOM_SOUP_LORE));
+        this.super_magic_mushroom_soup.texture(ConfigValue.SUPER_MAGIC_MUSHROOM_SOUP_TEXTURE);
 
         this.builders_wand = new ItemBuilder(Material.BLAZE_ROD);
-        this.builders_wand.setItemName(Utilis.color(GeneralConfig.BUILDERS_WAND_NAME));
-        this.builders_wand.setItemLore(Utilis.color(GeneralConfig.BUILDERS_WAND_LORE));
+        this.builders_wand.setItemName(Utilis.color(ConfigValue.BUILDERS_WAND_NAME));
+        this.builders_wand.setItemLore(Utilis.color(ConfigValue.BUILDERS_WAND_LORE));
 
         this.grappling_hook = new ItemBuilder(Material.FISHING_ROD);
-        this.builders_wand.setItemName(Utilis.color(GeneralConfig.GRAPPLING_HOOK_NAME));
-        this.builders_wand.setItemLore(Utilis.color(GeneralConfig.GRAPPLING_HOOK_LORE));
+        this.builders_wand.setItemName(Utilis.color(ConfigValue.GRAPPLING_HOOK_NAME));
+        this.builders_wand.setItemLore(Utilis.color(ConfigValue.GRAPPLING_HOOK_LORE));
 
         this.items = new ItemBuilder[] {
                 this.magic_mushroom_soup, this.super_magic_mushroom_soup, this.builders_wand, this.grappling_hook
